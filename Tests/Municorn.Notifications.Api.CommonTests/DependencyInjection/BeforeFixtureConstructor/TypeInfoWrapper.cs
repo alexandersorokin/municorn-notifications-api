8fc31b865cca13948e4673fc81173a9e77b45c8a
Alexander Sorokin <you@example.com>
2022-06-21 00:49:43 +0500

    Wrapper is simplified

--- Tests/Municorn.Notifications.Api.CommonTests/DependencyInjection/BeforeFixtureConstructor/TypeInfoWrapper.cs
+++ Tests/Municorn.Notifications.Api.CommonTests/DependencyInjection/BeforeFixtureConstructor/TypeInfoWrapper.cs
@@ -377,19 +377,17 @@ namespace Municorn.Notifications.Api.Tests.DependencyInjection.BeforeFixtureCons
             private ServiceProvider GetServiceProvider(ITest test) => this.serviceProvider ?? throw new InvalidOperationException($"Service provider is not initialized for {test.FullName}");
         }
 
-        private class ReplaceTestBuilderMethodWrapper : IMethodInfo
+        private class ReplaceTestBuilderMethodWrapper : MethodWrapper, IReflectionInfo
         {
-            private readonly IMethodInfo implementation;
-
-            public ReplaceTestBuilderMethodWrapper(IMethodInfo implementation)
+            public ReplaceTestBuilderMethodWrapper(IMethodInfo methodInfo)
+                : base(methodInfo.TypeInfo.Type, methodInfo.MethodInfo)
             {
-                this.implementation = implementation;
             }
 
-            public T[] GetCustomAttributes<T>(bool inherit)
+            T[] IReflectionInfo.GetCustomAttributes<T>(bool inherit)
                 where T : class
             {
-                var result = this.implementation.GetCustomAttributes<T>(inherit);
+                var result = this.GetCustomAttributes<T>(inherit);
                 if (typeof(T) == typeof(ITestBuilder))
                 {
                     return result
@@ -401,38 +399,6 @@ namespace Municorn.Notifications.Api.Tests.DependencyInjection.BeforeFixtureCons
 
                 return result;
             }
-
-            public bool IsDefined<T>(bool inherit)
-                where T : class =>
-                this.implementation.IsDefined<T>(inherit);
-
-            public IParameterInfo[] GetParameters() => this.implementation.GetParameters();
-
-            public Type[] GetGenericArguments() => this.implementation.GetGenericArguments();
-
-            public IMethodInfo MakeGenericMethod(params Type[] typeArguments) => this.implementation.MakeGenericMethod(typeArguments);
-
-            public object? Invoke(object? fixture, params object?[]? args) => this.implementation.Invoke(fixture, args);
-
-            public ITypeInfo TypeInfo => this.implementation.TypeInfo;
-
-            public MethodInfo MethodInfo => this.implementation.MethodInfo;
-
-            public string Name => this.implementation.Name;
-
-            public bool IsAbstract => this.implementation.IsAbstract;
-
-            public bool IsPublic => this.implementation.IsPublic;
-
-            public bool IsStatic => this.implementation.IsStatic;
-
-            public bool ContainsGenericParameters => this.implementation.ContainsGenericParameters;
-
-            public bool IsGenericMethod => this.implementation.IsGenericMethod;
-
-            public bool IsGenericMethodDefinition => this.implementation.IsGenericMethodDefinition;
-
-            public ITypeInfo ReturnType => this.implementation.ReturnType;
         }
 
         private class FixtureProvider : IFixtureProvider
