d5815019b540c6bd051f60c59a6c856b62ed39d8
Alexander Sorokin <you@example.com>
2022-06-21 01:01:55 +0500

    MethodWrapper is simplified

--- Tests/Municorn.Notifications.Api.CommonTests/DependencyInjection/ScopeMethodInject/UseContainerMethodInfo.cs
+++ Tests/Municorn.Notifications.Api.CommonTests/DependencyInjection/ScopeMethodInject/UseContainerMethodInfo.cs
@@ -1,37 +1,28 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Reflection;
 using Microsoft.Extensions.DependencyInjection;
 using NUnit.Framework.Interfaces;
+using NUnit.Framework.Internal;
 
 namespace Municorn.Notifications.Api.Tests.DependencyInjection.ScopeMethodInject
 {
-    [PrimaryConstructor]
-    internal partial class UseContainerMethodInfo : IMethodInfo
+    internal class UseContainerMethodInfo : MethodWrapper, IMethodInfo
     {
-        private readonly IMethodInfo implementation;
         private readonly IServiceProvider serviceProvider;
         private readonly object containerFixture;
 
-        public T[] GetCustomAttributes<T>(bool inherit)
-            where T : class =>
-            this.implementation.GetCustomAttributes<T>(inherit);
+        public UseContainerMethodInfo(IMethodInfo methodInfo, IServiceProvider serviceProvider, object containerFixture)
+            : base(methodInfo.TypeInfo.Type, methodInfo.MethodInfo)
+        {
+            this.serviceProvider = serviceProvider;
+            this.containerFixture = containerFixture;
+        }
 
-        public bool IsDefined<T>(bool inherit)
-            where T : class =>
-            this.implementation.IsDefined<T>(inherit);
-
-        public IParameterInfo[] GetParameters() => this.implementation.GetParameters();
-
-        public Type[] GetGenericArguments() => this.implementation.GetGenericArguments();
-
-        public IMethodInfo MakeGenericMethod(params Type[] typeArguments) => this.implementation.MakeGenericMethod(typeArguments);
-
-        public object? Invoke(object? fixture, params object?[]? args)
+        object? IMethodInfo.Invoke(object? fixture, params object?[]? args)
         {
             var resolvedArguments = this.ResolveArgs(fixture, args ?? Enumerable.Empty<object?>()).ToArray();
-            return this.implementation.Invoke(fixture, resolvedArguments);
+            return this.Invoke(fixture, resolvedArguments);
         }
 
         private IEnumerable<object?> ResolveArgs(object? methodFixture, IEnumerable<object?> args)
@@ -41,25 +32,5 @@ namespace Municorn.Notifications.Api.Tests.DependencyInjection.ScopeMethodInject
                 let serviceType = (arg as IInjectedService)?.GetServiceType(methodFixture, this.containerFixture)
                 select serviceType is null ? arg : this.serviceProvider.GetRequiredService(serviceType);
         }
-
-        public ITypeInfo TypeInfo => this.implementation.TypeInfo;
-
-        public MethodInfo MethodInfo => this.implementation.MethodInfo;
-
-        public string Name => this.implementation.Name;
-
-        public bool IsAbstract => this.implementation.IsAbstract;
-
-        public bool IsPublic => this.implementation.IsPublic;
-
-        public bool IsStatic => this.implementation.IsStatic;
-
-        public bool ContainsGenericParameters => this.implementation.ContainsGenericParameters;
-
-        public bool IsGenericMethod => this.implementation.IsGenericMethod;
-
-        public bool IsGenericMethodDefinition => this.implementation.IsGenericMethodDefinition;
-
-        public ITypeInfo ReturnType => this.implementation.ReturnType;
     }
 }
