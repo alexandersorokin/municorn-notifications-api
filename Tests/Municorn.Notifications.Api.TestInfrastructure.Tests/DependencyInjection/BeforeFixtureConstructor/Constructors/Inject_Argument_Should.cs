cd565192f6b953e4ccd2c67c3242eeafa47f4a97
Alexander Sorokin <you@example.com>
2022-06-22 02:02:25 +0500

    Argument injection tests

---
+++ Tests/Municorn.Notifications.Api.TestInfrastructure.Tests/DependencyInjection/BeforeFixtureConstructor/Constructors/Inject_Argument_Should.cs
@@ -0,0 +1,20 @@
+﻿using FluentAssertions;
+using Municorn.Notifications.Api.TestInfrastructure.DependencyInjection.BeforeFixtureConstructor;
+using NUnit.Framework;
+
+namespace Municorn.Notifications.Api.TestInfrastructure.Tests.DependencyInjection.BeforeFixtureConstructor.Constructors
+{
+    [TestFixtureInjectable("passed")]
+    internal class Inject_Argument_Should
+    {
+        private readonly string argument;
+
+        public Inject_Argument_Should(string argument) => this.argument = argument;
+
+        [Test]
+        public void Case()
+        {
+            this.argument.Should().Be("passed");
+        }
+    }
+}
---
+++ Tests/Municorn.Notifications.Api.TestInfrastructure.Tests/DependencyInjection/BeforeFixtureConstructor/Constructors/Inject_Service_Should.cs
@@ -0,0 +1,21 @@
+﻿using FluentAssertions;
+using Municorn.Notifications.Api.TestInfrastructure.DependencyInjection.BeforeFixtureConstructor;
+using Municorn.Notifications.Api.TestInfrastructure.DependencyInjection.ScopeAsyncLocal;
+using NUnit.Framework;
+
+namespace Municorn.Notifications.Api.TestInfrastructure.Tests.DependencyInjection.BeforeFixtureConstructor.Constructors
+{
+    [TestFixtureInjectable]
+    internal class Inject_Service_Should
+    {
+        private readonly AsyncLocalTestCaseServiceResolver service;
+
+        public Inject_Service_Should(AsyncLocalTestCaseServiceResolver service) => this.service = service;
+
+        [Test]
+        public void Case()
+        {
+            this.service.Should().NotBeNull();
+        }
+    }
+}
