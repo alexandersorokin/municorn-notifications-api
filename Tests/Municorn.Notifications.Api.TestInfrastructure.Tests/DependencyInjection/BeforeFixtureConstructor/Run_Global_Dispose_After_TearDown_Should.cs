708d36f9f31ac337534b9b6641490719b64009b8
Alexander Sorokin <you@example.com>
2022-06-22 13:10:25 +0500

    Test on order is added

---
+++ Tests/Municorn.Notifications.Api.TestInfrastructure.Tests/DependencyInjection/BeforeFixtureConstructor/Run_Global_Dispose_After_TearDown_Should.cs
@@ -0,0 +1,44 @@
+﻿using FluentAssertions;
+using Municorn.Notifications.Api.TestInfrastructure.DependencyInjection.BeforeFixtureConstructor;
+using NUnit.Framework;
+
+namespace Municorn.Notifications.Api.TestInfrastructure.Tests.DependencyInjection.BeforeFixtureConstructor
+{
+    [TestFixtureInjectable]
+    [LogModule]
+    [ParameterModule]
+    [AdHocModule(typeof(FixtureTimeLogger))]
+    internal class Run_Global_Dispose_After_TearDown_Should
+    {
+        private readonly Counter counter;
+
+        public Run_Global_Dispose_After_TearDown_Should([Register] Counter counter) => this.counter = counter;
+
+        [OneTimeSetUp]
+        public void OneTimeSetUp(FixtureTimeLogger fixtureTimeLogger)
+        {
+            fixtureTimeLogger.Run();
+        }
+
+        [Test]
+        [Repeat(2)]
+        public void Case()
+        {
+            true.Should().BeTrue();
+        }
+
+        [TestCase(10)]
+        [TestCase(11)]
+        [Repeat(2)]
+        public void Cases(int value)
+        {
+            value.Should().BePositive();
+        }
+
+        [OneTimeTearDown]
+        public void Before_Container_Dispose()
+        {
+            this.counter.Value.Should().Be(0);
+        }
+    }
+}
