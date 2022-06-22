27a24de9f7efbdf39cbb8bb67d35528a9de5043e
Alexander Sorokin <you@example.com>
2022-06-22 14:16:55 +0500

    Test for before test

---
+++ Tests/Municorn.Notifications.Api.TestInfrastructure.Tests/DependencyInjection/BeforeFixtureConstructor/Create_Scope_Before_Apply_To_Context_Should.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using FluentAssertions;
+using Microsoft.Extensions.DependencyInjection;
+using Municorn.Notifications.Api.TestInfrastructure.DependencyInjection;
+using Municorn.Notifications.Api.TestInfrastructure.DependencyInjection.AutoMethods;
+using Municorn.Notifications.Api.TestInfrastructure.DependencyInjection.BeforeFixtureConstructor;
+using Municorn.Notifications.Api.TestInfrastructure.DependencyInjection.ScopeMethodInject;
+using NUnit.Framework;
+using NUnit.Framework.Interfaces;
+using NUnit.Framework.Internal;
+
+namespace Municorn.Notifications.Api.TestInfrastructure.Tests.DependencyInjection.BeforeFixtureConstructor
+{
+    [TestFixtureInjectable]
+    [TestTimeLoggerModule]
+    internal class Create_Scope_Before_Apply_To_Context_Should
+    {
+        [Test]
+        [Repeat(2)]
+        [ApplyToContext]
+        public void Case([Inject] IFixtureSetUp service)
+        {
+            service.Should().NotBeNull();
+        }
+
+        [TestCase(10)]
+        [TestCase(11)]
+        [ApplyToContext]
+        [Repeat(2)]
+        public void Cases(int value, [Inject] IFixtureSetUp service)
+        {
+            service.Should().NotBeNull();
+        }
+
+        [AttributeUsage(AttributeTargets.Method)]
+        private sealed class ApplyToContextAttribute : NUnitAttribute, IApplyToContext
+        {
+            public void ApplyToContext(TestExecutionContext context)
+            {
+                var test = context.CurrentTest;
+                if (!test.IsSuite)
+                {
+                    test
+                        .GetServiceProvider(context.TestObject)
+                        .GetRequiredService<IFixtureSetUp>()
+                        .Should()
+                        .NotBeNull();
+                }
+            }
+        }
+    }
+}
