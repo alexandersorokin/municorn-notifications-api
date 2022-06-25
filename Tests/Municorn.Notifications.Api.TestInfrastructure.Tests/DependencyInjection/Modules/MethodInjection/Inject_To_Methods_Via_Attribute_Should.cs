954691a70d9f885e33104b1aa9a793093995a2b4
Alexander Sorokin <you@example.com>
2022-06-25 22:03:43 +0500

    Method inject attribute test

---
+++ Tests/Municorn.Notifications.Api.TestInfrastructure.Tests/DependencyInjection/Modules/MethodInjection/Inject_To_Methods_Via_Attribute_Should.cs
@@ -0,0 +1,24 @@
+﻿using FluentAssertions;
+using Microsoft.Extensions.DependencyInjection;
+using Municorn.Notifications.Api.TestInfrastructure.DependencyInjection.Modules.Abstractions;
+using Municorn.Notifications.Api.TestInfrastructure.DependencyInjection.Modules.MethodInjection;
+using NUnit.Framework;
+using NUnit.Framework.Internal;
+using Vostok.Logging.Abstractions;
+
+namespace Municorn.Notifications.Api.TestInfrastructure.Tests.DependencyInjection.Modules.MethodInjection
+{
+    [TestMethodInjectionModule]
+    internal class Inject_To_Methods_Via_Attribute_Should : FrameworkServiceProviderFixtureBase
+    {
+        public Inject_To_Methods_Via_Attribute_Should()
+            : base(serviceCollection => serviceCollection
+                .AddFixtureServiceCollectionModuleAttributes(new TypeWrapper(typeof(Inject_To_Methods_Via_Attribute_Should)))
+                .AddSingleton(new SilentLog()))
+        {
+        }
+
+        [Test]
+        public void Simple_Inject([InjectDependency] SilentLog service) => service.Should().NotBeNull();
+    }
+}
