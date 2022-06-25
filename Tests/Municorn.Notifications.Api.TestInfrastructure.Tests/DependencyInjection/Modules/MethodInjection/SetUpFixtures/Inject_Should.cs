ba22b47759ec5fa35333f3349c7d121b24f02e9b
Alexander Sorokin <you@example.com>
2022-06-25 22:22:37 +0500

    SetUpFixture tests

---
+++ Tests/Municorn.Notifications.Api.TestInfrastructure.Tests/DependencyInjection/Modules/MethodInjection/SetUpFixtures/Inject_Should.cs
@@ -0,0 +1,23 @@
+﻿using FluentAssertions;
+using Microsoft.Extensions.DependencyInjection;
+using Municorn.Notifications.Api.TestInfrastructure.DependencyInjection.Modules.MethodInjection;
+using NUnit.Framework;
+using Vostok.Logging.Abstractions;
+
+namespace Municorn.Notifications.Api.TestInfrastructure.Tests.DependencyInjection.Modules.MethodInjection.SetUpFixtures
+{
+    [TestFixture]
+    internal class Inject_Should : FrameworkServiceProviderFixtureBase
+    {
+        public Inject_Should()
+            : base(serviceCollection => serviceCollection
+                .AddTestMethodInjection()
+                .AddSingleton(new SilentLog()))
+        {
+        }
+
+        [Test]
+        [Repeat(2)]
+        public void Inject_Service([InjectDependency] SilentLog service) => service.Should().NotBeNull();
+    }
+}
---
+++ Tests/Municorn.Notifications.Api.TestInfrastructure.Tests/DependencyInjection/Modules/MethodInjection/SetUpFixtures/SetUpFixture.cs
@@ -0,0 +1,9 @@
+﻿using NUnit.Framework;
+
+namespace Municorn.Notifications.Api.TestInfrastructure.Tests.DependencyInjection.Modules.MethodInjection.SetUpFixtures
+{
+    [SetUpFixture]
+    internal class SetUpFixture
+    {
+    }
+}
