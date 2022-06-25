f118c802ffb0ef69a5a494dd1a9172a7881fb9d1
Alexander Sorokin <you@example.com>
2022-06-25 14:57:47 +0500

    Duplicate registration is removed

--- Tests/Municorn.Notifications.Api.CommonTests/ApiTests/IServiceClientFixture.cs
+++ Tests/Municorn.Notifications.Api.CommonTests/ApiTests/IServiceClientFixture.cs
@@ -1,12 +1,10 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
 using Municorn.Notifications.Api.TestInfrastructure.DependencyInjection.AfterFixtureConstructor;
-using Municorn.Notifications.Api.TestInfrastructure.DependencyInjection.Modules.FieldInjection;
 using Municorn.Notifications.Api.TestInfrastructure.Logging;
 using Vostok.Logging.Abstractions;
 
 namespace Municorn.Notifications.Api.Tests.ApiTests
 {
-    [FieldInjectionModule]
     internal interface IServiceClientFixture : IFixtureWithServiceProviderFramework
     {
         void IFixtureWithServiceProviderFramework.ConfigureServices(IServiceCollection serviceCollection) => serviceCollection
