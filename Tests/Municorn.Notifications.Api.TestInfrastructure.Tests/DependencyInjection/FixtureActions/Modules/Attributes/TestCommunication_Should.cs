789992753b242012443096dda7b24b9e98e88421
Alexander Sorokin <you@example.com>
2022-06-26 14:37:47 +0500

    Test communication attribute tests

---
+++ Tests/Municorn.Notifications.Api.TestInfrastructure.Tests/DependencyInjection/FixtureActions/Modules/Attributes/TestCommunication_Should.cs
@@ -0,0 +1,20 @@
+﻿using FluentAssertions;
+using Microsoft.Extensions.DependencyInjection;
+using Municorn.Notifications.Api.TestInfrastructure.DependencyInjection.FixtureActions;
+using Municorn.Notifications.Api.TestInfrastructure.DependencyInjection.Modules.TestCommunication;
+using NUnit.Framework;
+
+namespace Municorn.Notifications.Api.TestInfrastructure.Tests.DependencyInjection.FixtureActions.Modules.Attributes
+{
+    [TestFixture]
+    [TestCommunicationModule]
+    internal class TestCommunication_Should : IFixtureWithServiceProviderFramework
+    {
+        public void ConfigureServices(IServiceCollection serviceCollection) => serviceCollection
+            .AddScoped<MockService>();
+
+        [Test]
+        [Repeat(2)]
+        public void Case() => this.GetRequiredService<MockService>().Should().NotBeNull();
+    }
+}
