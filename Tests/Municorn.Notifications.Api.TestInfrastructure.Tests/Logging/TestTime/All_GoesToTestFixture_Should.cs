90184a7d2efb12583de727b07535898a0ad8b89a
Alexander Sorokin <you@example.com>
2022-06-26 22:48:15 +0500

    TestTime log tests are completed

---
+++ Tests/Municorn.Notifications.Api.TestInfrastructure.Tests/Logging/TestTime/All_GoesToTestFixture_Should.cs
@@ -0,0 +1,23 @@
+﻿using FluentAssertions;
+using Microsoft.Extensions.DependencyInjection;
+using Municorn.Notifications.Api.TestInfrastructure.DependencyInjection.FixtureActions;
+using NUnit.Framework;
+
+namespace Municorn.Notifications.Api.TestInfrastructure.Tests.Logging.TestTime
+{
+    [TestFixture]
+    internal class All_GoesToTestFixture_Should : IFixtureWithServiceProviderFramework
+    {
+        public void ConfigureServices(IServiceCollection serviceCollection) =>
+            serviceCollection
+                .AddBoundLog()
+                .AddTestTimeLogger();
+
+        [Test]
+        public void Test() => true.Should().BeTrue();
+
+        [TestCase(1)]
+        [TestCase(2)]
+        public void TestCase(int value) => value.Should().BePositive();
+    }
+}
---
+++ Tests/Municorn.Notifications.Api.TestInfrastructure.Tests/Logging/TestTime/All_GoesToTest_Should.cs
@@ -0,0 +1,23 @@
+﻿using FluentAssertions;
+using Microsoft.Extensions.DependencyInjection;
+using Municorn.Notifications.Api.TestInfrastructure.DependencyInjection.FixtureActions;
+using NUnit.Framework;
+
+namespace Municorn.Notifications.Api.TestInfrastructure.Tests.Logging.TestTime
+{
+    [TestFixture]
+    internal class All_GoesToTest_Should : IFixtureWithServiceProviderFramework
+    {
+        public void ConfigureServices(IServiceCollection serviceCollection) =>
+            serviceCollection
+                .AddContextualLog()
+                .AddTestTimeLogger();
+
+        [Test]
+        public void Test() => true.Should().BeTrue();
+
+        [TestCase(1)]
+        [TestCase(2)]
+        public void TestCase(int value) => value.Should().BePositive();
+    }
+}
