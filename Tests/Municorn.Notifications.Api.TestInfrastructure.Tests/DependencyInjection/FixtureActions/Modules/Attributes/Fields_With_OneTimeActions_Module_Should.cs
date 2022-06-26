95bb484355f065e99b6b4abd210e606eea5a4cd0
Alexander Sorokin <you@example.com>
2022-06-26 11:58:34 +0500

    Combined attribute test is improved

--- Tests/Municorn.Notifications.Api.TestInfrastructure.Tests/DependencyInjection/FixtureActions/Modules/Attributes/Fields_With_OneTimeActions_Module_Should.cs
+++ Tests/Municorn.Notifications.Api.TestInfrastructure.Tests/DependencyInjection/FixtureActions/Modules/Attributes/Fields_With_OneTimeActions_Module_Should.cs
@@ -2,23 +2,25 @@
 using Microsoft.Extensions.DependencyInjection;
 using Municorn.Notifications.Api.TestInfrastructure.DependencyInjection.FixtureActions;
 using Municorn.Notifications.Api.TestInfrastructure.DependencyInjection.Modules.FieldInjection;
+using Municorn.Notifications.Api.TestInfrastructure.DependencyInjection.Modules.FixtureOneTimeActions;
 using NUnit.Framework;
-using Vostok.Logging.Abstractions;
 
 namespace Municorn.Notifications.Api.TestInfrastructure.Tests.DependencyInjection.FixtureActions.Modules.Attributes
 {
     [TestFixture]
     [FieldInjectionWithFixtureOneTimeActionsModule]
-    internal class Fields_With_OneTimeActions_Module_Should : IFixtureWithServiceProviderFramework
+    internal class Fields_With_OneTimeActions_Module_Should : IFixtureWithServiceProviderFramework, IOneTimeSetUpAction
     {
         [FieldDependency]
-        private readonly SilentLog service = default!;
+        private readonly Counter service = default!;
 
         public void ConfigureServices(IServiceCollection serviceCollection) => serviceCollection
-            .AddSingleton<SilentLog>();
+            .AddSingleton<Counter>();
+
+        public void OneTimeSetUp() => this.service.Increment();
 
         [Test]
         [Repeat(2)]
-        public void Case() => this.service.Should().NotBeNull();
+        public void Case() => this.service.Value.Should().Be(1);
     }
 }
