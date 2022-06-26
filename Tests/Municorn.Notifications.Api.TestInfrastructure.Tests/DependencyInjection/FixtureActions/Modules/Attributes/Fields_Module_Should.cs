94e5af922889523d2af10a79ba6b66e63cc61cbf
Alexander Sorokin <you@example.com>
2022-06-26 11:56:00 +0500

    new attribute tests are added

---
+++ Tests/Municorn.Notifications.Api.TestInfrastructure.Tests/DependencyInjection/FixtureActions/Modules/Attributes/Fields_Module_Should.cs
@@ -0,0 +1,24 @@
+﻿using FluentAssertions;
+using Microsoft.Extensions.DependencyInjection;
+using Municorn.Notifications.Api.TestInfrastructure.DependencyInjection.FixtureActions;
+using Municorn.Notifications.Api.TestInfrastructure.DependencyInjection.Modules.FieldInjection;
+using NUnit.Framework;
+using Vostok.Logging.Abstractions;
+
+namespace Municorn.Notifications.Api.TestInfrastructure.Tests.DependencyInjection.FixtureActions.Modules.Attributes
+{
+    [TestFixture]
+    [FieldInjectionModule]
+    internal class Fields_Module_Should : IFixtureWithServiceProviderFramework
+    {
+        [FieldDependency]
+        private readonly SilentLog service = default!;
+
+        public void ConfigureServices(IServiceCollection serviceCollection) => serviceCollection
+            .AddSingleton<SilentLog>();
+
+        [Test]
+        [Repeat(2)]
+        public void Case() => this.service.Should().NotBeNull();
+    }
+}
---
+++ Tests/Municorn.Notifications.Api.TestInfrastructure.Tests/DependencyInjection/FixtureActions/Modules/Attributes/OneTimeActions_Module_Should.cs
@@ -0,0 +1,19 @@
+﻿using FluentAssertions;
+using Municorn.Notifications.Api.TestInfrastructure.DependencyInjection.Modules.FixtureOneTimeActions;
+using NUnit.Framework;
+
+namespace Municorn.Notifications.Api.TestInfrastructure.Tests.DependencyInjection.FixtureActions.Modules.Attributes
+{
+    [TestFixture]
+    [FixtureOneTimeActionsModule]
+    internal class OneTimeActions_Module_Should : IWithoutServices, IOneTimeSetUpAction
+    {
+        private readonly Counter counter = new();
+
+        public void OneTimeSetUp() => this.counter.Increment();
+
+        [Test]
+        [Repeat(2)]
+        public void Case() => this.counter.Value.Should().Be(1);
+    }
+}
